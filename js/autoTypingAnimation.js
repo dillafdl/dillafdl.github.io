c65a88e71011c134f7b205d44aad141a147db188
dillafdl <125528378+dillafdl@users.noreply.github.com>
2023-06-27 14:52:05 +0700

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>I am an Electrical Engineer from Indonesia University of Education. During my studies, I had hands-on experience in designing and analyzing circuits, utilizing software tools such as MATLAB, Multisim, Cisco Packet Tracer, CST Studio Suite, Autocad, and several other software, and conducting experiments in a laboratory setting. I also have interest in programming languages such as HTML, CSS, and Python. I am a dedicated, organized, and responsible individual. I have good communication skills, am an excellent team worker and am keen and very willing to learn and develop new skills.</span> "
+      ],
+      typeSpeed: 0,
+      contentType: 'html'
+    });
+  }, 100);
